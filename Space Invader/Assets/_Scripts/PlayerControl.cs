c6925bba39c83df8223ba6aef6d4d60e8995f80d
Tianming Xu <txu1@seas.upenn.edu>
2020-01-22 20:25:03 -0500

    Space Invader add package and movement

---
+++ Space Invader/Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//customized class must be able to serialize to the main gameobject
+[System.Serializable]
+public class Boundary
+{
+    public float x_min, x_max;
+}
+public class PlayerControl : MonoBehaviour
+{
+    //public Variables
+    public float move_speed = 0.3f;
+    public float revive_time = 3.0f;
+    public Boundary boundary;
+    public float tilt;
+
+    //related gameobject -- need to be initialized in editor
+    public GameObject laser;
+
+    //private variables
+    bool isShot = false; //determine whether the player's ship has shot, if so, can't shoot until the bullet destroyed
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //fixed update to update the movement of ship
+    private void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            gameObject.transform.Translate(move_speed, 0, 0);
+            //we also want to have a tilt effect -- tilt should be reverse the movement direction to 
+            gameObject.transform.rotation = Quaternion.Euler(0.0f, 0.0f, -tilt);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            gameObject.transform.Translate(-move_speed, 0, 0);
+            gameObject.transform.rotation = Quaternion.Euler(0.0f, 0.0f, tilt);
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            //reset the rotation
+            gameObject.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        }
+
+        //can't exceed the boundary
+        //we need only limit the x-axis because player can only move left to right
+        float x_pos = Mathf.Clamp(gameObject.transform.position.x, boundary.x_min, boundary.x_max);
+        gameObject.transform.position = new Vector3(x_pos, 0.0f, gameObject.transform.position.z);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
