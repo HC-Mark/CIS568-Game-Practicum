4a9495af98a403e2870547769d9ac01916646579
Tianming Xu <txu1@seas.upenn.edu>
2020-01-23 12:36:35 -0500

    update asteroid feature

---
+++ Space Invader/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AsteroidSpawn
+{
+    public float asteroid_wait_min, asteroid_wait_max;
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject asteroid;
+    public GameObject enemy;
+    public Vector3 asteroid_spawn_value;
+    public bool asteroid_on_scene;
+    public int enemy_num;
+    //corountine setup
+    public AsteroidSpawn asteroid_spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //we need to set up coroutine to spawn it infintely
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: switch to based on probability
+        if (!asteroid_on_scene)
+        {
+            StartCoroutine(SpawnAsteroids());
+        }
+    }
+
+    IEnumerator SpawnAsteroids() {
+        asteroid_on_scene = true;
+        float asteroid_wait = Random.Range(asteroid_spawn.asteroid_wait_min, asteroid_spawn.asteroid_wait_max);
+        yield return new WaitForSeconds(asteroid_wait);
+        Vector3 spawn_position = asteroid_spawn_value;
+        Quaternion spawn_rotation = Quaternion.identity;
+        Instantiate(asteroid, spawn_position, spawn_rotation);
+    }
+}
--- Space Invader/Assets/_Scripts/PlayerLaser.cs
+++ Space Invader/Assets/_Scripts/PlayerLaser.cs
@@ -6,13 +6,17 @@ public class PlayerLaser : MonoBehaviour
 {
     public float speed = 2;
     public GameObject player_ship;
+    public GameObject asteroid_explosion;
+    public GameObject enemy_explosion;
 
+    private GameObject game_manager;
     private Rigidbody rd;
     // Start is called before the first frame update
     void Start()
     {
 
         rd = GetComponent<Rigidbody>();
+        game_manager = GameObject.Find("GameManager");
         //gameObject.transform.Translate(0.0f, 0.0f, speed);
         rd.velocity = transform.forward * speed;
 
@@ -28,11 +32,24 @@ public class PlayerLaser : MonoBehaviour
     {
         if (other.tag == "Boundary")
         {
-            Die();
+            LaserDie();
         }
     }
 
-    void Die()
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Asteroid")
+        {
+            //update score here to game manager
+            other.GetComponent<Asteroid>().AsteroidDie();
+            //need to clean up the left effect object ?
+            //create explosion effect
+            Instantiate(asteroid_explosion, transform.position, transform.rotation);
+            LaserDie();
+        }
+    }
+
+    void LaserDie()
     {
         player_ship.GetComponent<PlayerControl>().is_shot = false;
         Destroy(gameObject);
